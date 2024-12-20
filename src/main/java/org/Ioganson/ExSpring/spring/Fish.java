4d9accf997bb5b363c61aa8968bb46cf76edd7d0
Anton Ioganson <Two52@ya.ru>
2024-12-20 16:34:14 +0300

    new

--- src/main/java/org/Ioganson/ExSpring/spring/Fish.java
+++ src/main/java/org/Ioganson/ExSpring/spring/Fish.java
@@ -1,10 +1,15 @@
 package org.Ioganson.ExSpring.spring;
 
 public class Fish {
+    private String name;
     public void eat() {
         System.out.println("Eat fish");
     }
     public void sleep() {
         System.out.println("Sleep fish");
     }
+    public String getName() {
+        return name;
+    }
+
 }
