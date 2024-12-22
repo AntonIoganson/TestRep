f38ca86419f284f7317075e32d426071bb9f4c57
Anton Ioganson <Two52@ya.ru>
2024-12-22 15:03:58 +0300

    add hount

---
+++ src/main/java/org/Ioganson/ExSpring/NewFeature/Hounting.java
@@ -0,0 +1,11 @@
+package org.Ioganson.ExSpring.NewFeature;
+
+public class Hounting {
+    public void hount() {
+        System.out.println("Hounting...");
+
+    }
+    public void sled(){
+        System.out.println("go to sled");
+    }
+}
--- src/main/java/org/Ioganson/ExSpring/Test1.java
+++ src/main/java/org/Ioganson/ExSpring/Test1.java
@@ -1,5 +1,7 @@
 package org.Ioganson.ExSpring;
 
+
+
 public class Test1 {
     public static void main(String[] args) {
         Pat pat = new Cat();
