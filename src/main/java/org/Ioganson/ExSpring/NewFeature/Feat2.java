86fe92414c633b53c79123157ed57bb6fb206506
Anton Ioganson <Two52@ya.ru>
2024-12-23 10:44:16 +0300

    add super jump

--- src/main/java/org/Ioganson/ExSpring/NewFeature/Feat2.java
+++ src/main/java/org/Ioganson/ExSpring/NewFeature/Feat2.java
@@ -4,4 +4,10 @@ public class Feat2 {
     public void steal(){
         System.out.println("isSteal");
     }
+    public void jump(){
+        System.out.println("jump");
+    }
+    public void superJump(){
+        System.out.println("Super jump");
+    }
 }
